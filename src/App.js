987b97e8f7f5514d14a720c610e1eafaa21c7ffa
Jad Shaker <jaadshaker@gmail.com>
2021-08-15 17:00:08 +0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React App</h1>
+      <h2>Author: Jad Shaker</h2>
+    </div>
+  );
+}
+
+export default App;
